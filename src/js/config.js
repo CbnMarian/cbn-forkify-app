fb017423e5876fd956ba14707526f372d2bb07e7
CbnMarian <m.marian03@yahoo.com>
2023-10-18 13:37:16 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+'use strict';
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a57ea7e7-41ee-4767-ae28-d5b31e519104';
+export const MODAL_CLOSE_SEC = 2.5;
